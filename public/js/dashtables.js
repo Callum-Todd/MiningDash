c933e790ae45584cdcf8a133f98213518be9a9ee
Callum Todd <callumtxdd@gmail.com>
2021-03-25 11:24:35 +0000

    update

---
+++ public/js/dashtables.js
@@ -0,0 +1,69 @@
+fetch("db.json")
+.then(response => response.json())
+.then(data => {
+
+    document.getElementById('ticker').innerHTML = "£" + ((data.walletbalance/1000000000000000000) * data.price).toFixed(2);
+
+
+    const pTable = document.getElementById('paymentstable');
+    const wTable = document.getElementById('workertable');
+    const wdTable = document.getElementById('workhisttable');
+
+
+    for (let index = data.payments.length - 1; index >= 0; index--) {
+        const item = data.payments[index];
+        let row = pTable.insertRow();
+        let date = row.insertCell(0);
+        date.innerHTML = "<a class=\"datelink\" href=\"https://etherscan.io/tx/" + item.txid + "\">" + item.date + "</a>";
+        let amount = row.insertCell(1);
+        amount.innerHTML = (item.amount/1000000000000000000).toFixed(7) + " Ξ";
+        let value = row.insertCell(2);
+        value.innerHTML = (item.amount/1000000000000000000 * item.price).toFixed(2);
+        let price = row.insertCell(3);
+        price.innerHTML = item.price;
+        price.textContent = (+price.textContent).toLocaleString('en-US', { style: 'currency', currency: 'GBP' });
+        value.textContent = (+value.textContent).toLocaleString('en-US', { style: 'currency', currency: 'GBP' });
+    }
+
+
+    let sum = 0;
+    data.workers.forEach(item =>{ 
+        sum += item.valid_shares;
+    })
+    console.log(sum);
+
+    for (let index = data.workers.length - 1; index >= 0; index--) {
+        const item = data.workers[index];
+        let row = wTable.insertRow();
+        let name = row.insertCell(0);
+        let valid = row.insertCell(1);
+        let stale = row.insertCell(2);
+        let share = row.insertCell(3);
+        let online = row.insertCell(4);
+
+        name.innerHTML = item.name;
+        valid.innerHTML = item.valid_shares;
+        stale.innerHTML = item.stale_shares;
+        online.innerHTML = item.online;
+        share.innerHTML = ((item.valid_shares / sum) * 100 ).toFixed(2) + '%' ;
+        
+    }
+
+    for (let index = data.shares_history.length - 1; index >= 0; index--) {
+        const item = data.shares_history[index];
+        let row = wdTable.insertRow();
+        let date = row.insertCell(0);
+        let rigT = row.insertCell(1);
+        let andrasT = row.insertCell(2);
+        let callumT = row.insertCell(3);
+        let markT = row.insertCell(4);
+        let Total = row.insertCell(5);
+        date.innerHTML = item.date;
+        rigT.innerHTML = item.rig;
+        andrasT.innerHTML = item.andras;
+        callumT.innerHTML = item.callum;
+        markT.innerHTML = item.mark;
+        Total.innerHTML = item.total;
+    }
+
+})
--- public/js/projections.js
+++ public/js/projections.js
@@ -10,11 +10,23 @@ fetch("db.json")
     });
     let average_daily = sum / count;
     average_daily = ((average_daily/1000000000000000000) * data.price);
+
+    let cost_daily = 4.57;
     document.getElementById("daily").innerHTML = "£" + average_daily.toFixed(2);
     document.getElementById("weekly").innerHTML = "£" + (average_daily * 7).toFixed(2);
     document.getElementById("monthly").innerHTML = "£" + (average_daily * 28).toFixed(2);
     document.getElementById("annual").innerHTML = "£" + (average_daily * 365).toFixed(2);
     document.getElementById("priceline").innerHTML += "£" + data.price;
+
+    document.getElementById("cost_daily").innerHTML = "£" + (cost_daily).toFixed(2);
+    document.getElementById("cost_weekly").innerHTML = "£" + (cost_daily*7).toFixed(2);
+    document.getElementById("cost_monthly").innerHTML = "£" + (cost_daily*28).toFixed(2);
+    document.getElementById("cost_annual").innerHTML = "£" + (cost_daily*365).toFixed(2);
+
+    document.getElementById("profit_daily").innerHTML = "£" + (average_daily - cost_daily).toFixed(2);
+    document.getElementById("profit_weekly").innerHTML = "£" + (average_daily*7 - cost_daily*7).toFixed(2);
+    document.getElementById("profit_monthly").innerHTML = "£" + (average_daily*28 - cost_daily*28).toFixed(2);
+    document.getElementById("profit_annual").innerHTML = "£" + (average_daily*365 - cost_daily*365).toFixed(2);
 
 
 

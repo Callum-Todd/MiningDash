a4f3b0bf39fa5436fbef82db596a3268a1f4e54d
Callum Todd <callumtxdd@gmail.com>
2021-04-03 14:02:47 +0100

    updated host.js, dashtables.js and index.html

--- host.js
+++ host.js
@@ -5,7 +5,7 @@ var fetch = require('cross-fetch');
 var path = require('path');
 var schedule = require('node-schedule');
 var date = require('date-and-time');
-var port = 8080;
+var port = 3000;
 
 app.use(express.static(__dirname + '/public'));
 app.use(function (req, res, next) {
@@ -28,18 +28,21 @@ function update() {
     .then(pricedata => {
         db.price = pricedata.GBP;
     })
-    fetch("https://api.ethermine.org/miner/d1c6ddd842180cd54eee389aa1302bcaf55fa44a/workers")
-    .then(response => response.json())
-    .then(data => {
-        db.workers = data.result;
-    })
+    // fetch("https://api.ethermine.org/miner/d1c6ddd842180cd54eee389aa1302bcaf55fa44a/workers")
+    // .then(response => response.json())
+    // .then(workerData => {
+    //     db.workers = workerData.result.data[0];
+    // })
     fetch("https://api.ethermine.org/miner/d1c6ddd842180cd54eee389aa1302bcaf55fa44a/dashboard")
         .then(response => response.json())
-        .then(data => {
-            if (data.result == null) return;
-            db.hashrate = data.result.currentStatistics.reportedHashrate;
-            db.poolbalance = data.result.currentStatistics.unpaid;
+        .then(dashboardData => {
+            // if (dashboardData.result == null) return;
+            // console.log("reported hashrate == " + dashboardData.result.data.currentStatistics.reportedHashrate);
+            db.hashrate = dashboardData.data.currentStatistics.reportedHashrate;
+            db.poolbalance = dashboardData.data.currentStatistics.unpaid;
+            db.workers = dashboardData.data.workers;
     })
+    save();
     // fetch("https://flexpool.io/api/v1/pool/currentLuck")
     //     .then(response => response.json())
     //     .then(data => {
@@ -47,36 +50,29 @@ function update() {
     //             db.luck_history.shift();
     //         }
     //             db.luck_history.push(data.result);
-
-    // // })
-    // fetch("https://flexpool.io/api/v1/miner/0xedD0CaF72bC3bc30c185Db4066a55102eD54A01F/balance")
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         db.poolbalance = data.result;
-    // })
         
-    fetch("https://api.ethermine.org/miner/d1c6ddd842180cd54eee389aa1302bcaf55fa44a/payouts")
-        .then(response =>response.json())
-        .then(data => {
-            if (data.result.data == null) {
-                return;
-            }
-            data.result.data.forEach(item => {
-                let found = false;
-                db.payments.forEach(ele => {
-                    if (item.txHash == ele.txid) {
-                        found = true;
-                    }
-                })
-                if (!found) {
-                    let now = new Date();
-                    db.payments.push({  "amount" : item.amount,
-                                        "price" : db.price,
-                                        "date" : date.format(now, 'DD/MM/YYYY'),
-                                        "txid" : item.txHash)
-                }
-            })
-        })
+    // fetch("https://api.ethermine.org/miner/d1c6ddd842180cd54eee389aa1302bcaf55fa44a/payouts")
+    //     .then(response =>response.json())
+    //     .then(payoutData => {
+    //         // if (data.result.data == null) {
+    //         //     return;
+    //         // }
+    //         payoutData.result.data.forEach(item => {
+    //             let found = false;
+    //             db.payments.forEach(ele => {
+    //                 if (item.txHash == ele.txid) {
+    //                     found = true;
+    //                 }
+    //             })
+    //             if (!found) {
+    //                 let now = new Date();
+    //                 db.payments.push({  "amount" : item.amount,
+    //                                     "price" : db.price,
+    //                                     "date" : date.format(now, 'DD/MM/YYYY'),
+    //                                     "txid" : item.txHash})
+    //             }
+    //         })
+    //     })
 
     if (db.hash_history.length >= 50) {
         db.hash_history.shift();
@@ -87,8 +83,9 @@ function update() {
         db.hash_history.push(db.hashrate/1000000);
     }
 }
+
 update();    
-save();
+// save();
 
 // ------------------------------------------------------------>
 // Routing
@@ -124,15 +121,41 @@ setTimeout(() => {  console.log(db);
 
 setInterval(() => { 
     console.log("<-------------- Printing JSON -------------->"); 
-    console.log(db);
-    jsonfile.writeFile('./public/db.json', db, function (err) {
-        if (err) console.error(err)
-    })
     update();
+    console.log(db);
+    save();
 }, 180000);
 
+const sharesUpdate = schedule.scheduleJob('0 */12 * * *', (firetime) => {
+    let [c, a, m, r] = [0,0,0,0];
+    db.workers.forEach(item => {
+        switch (item.worker){
+            case "battlemoira":
+                r = item.validShares;
+                break;
+            case "torbjorn":
+                a = item.validShares;
+                break;
+            case "markminer":
+                m = item.validShares;
+                break;
+            case "junkrat":
+                c = item.validShares;
+                break;
+        }
+    });
+    db.shares_buffer.push({
+        "rig" : r,
+        "andras" : a,
+        "callum" : c,
+        "mark" : m, 
+        "total" : r+a+c+m
+    })
+
+});
+
 // Daily Job executed at midnight
-const dailyJob = schedule.scheduleJob({hour: 23, minute: 59}, (firetime) => {
+const dailyJob = schedule.scheduleJob('5 */24 * * *', (firetime) => {
     update();
     console.log("Daily job ran @" + firetime);
     let diff;
@@ -145,23 +168,16 @@ const dailyJob = schedule.scheduleJob({hour: 23, minute: 59}, (firetime) => {
     let now = new Date();
     db.rewards.push({"amount" : diff, "price" : db.price, "date" : date.format(now, 'DD/MM/YYYY') });
     db.dailybalance = db.poolbalance;
+    
+    let first = db.shares_buffer[db.shares_buffer.length - 2];
+    let second = db.shares_buffer[db.shares_buffer.length - 1];
+
     let [c, a, m, r] = [0,0,0,0];
-    db.workers.forEach(item => {
-        switch (item.name){
-            case "BattleMoira":
-                r = item.valid_shares;
-                break;
-            case "Torbjorn":
-                a = item.valid_shares;
-                break;
-            case "MarkMiner":
-                m = item.valid_shares;
-                break;
-            case "Junkrat":
-                c = item.valid_shares;
-                break;
-        }
-    });
+    r = first.rig + second.rig;
+    m = first.mark + second.mark;
+    a = first.andras + second.andras;
+    c = first.callum + second.callum;
+
     db.shares_history.push({
         "date" : date.format(now, 'DD/MM/YYYY'),
         "rig" : r,
